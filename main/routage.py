701e82737edbae222f38761783608c7d62dde05f
Cedric Lamoriniere <cedric.lamoriniere@gmail.com>
2014-04-05 12:22:16 +0200

    update

--- main/routage.py
+++ main/routage.py
@@ -1,10 +1,28 @@
 import parser
+import algos
+
+import networkx as nx
 
 if __name__ == '__main__':
-	G, nbCars, Totaltime, intersections, streets = parser.parseFile("paris_54000.txt")
+	# Call parser
+	G = nx.Graph()
+	G, nbCars, Totaltime, intersections, streets, nodeStart = parser.parseFile("paris_54000.txt")
 	
 	print "totaltime : ", str(Totaltime)
 	print "nbCars : ", str(nbCars)
 	print "streets nb : ", str(len(streets))
 	print "intersections nb : ", str(len(intersections))
-        
+	
+	# street info a Street[Distance,Time]
+	
+	# get the start node 
+	_startNode = G.node[nodeStart]
+	
+	# loop on the graph for each car
+	for aCar in range(1,nbCars):
+		_startNode.items()
+		
+		
+		
+		
+	
