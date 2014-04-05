4fab66468e8858125a075cdebf1a4eaae0fbd81b
seguins <stephseguin93@gmail.com>
2014-04-05 11:45:36 +0200

    structure base parser

---
+++ main/parser.py
@@ -0,0 +1,23 @@
+import sys
+import networkx as nx
+
+def parseFile(inputFile):
+        inputFile = open(inputFile)
+        print "Name of the file : ", inputFile.name
+        lines = inputFile.readlines()
+
+
+        params = lines[0].split(" ")
+        nbIntersections = int(params[0])
+        nbStreets = int(params[1])
+        time = int(params[2])
+        nbCars = int(params[3])
+        numStartIntersection = int(params[4])
+
+        endIntersection = nbIntersections + 1
+        intersections = lines[1:endIntersection]
+        streets = lines[endIntersection +1 : endIntersection + 1 + nbStreets]
+
+        G=nx.Graph()
+	return G
+
---
+++ main/routage.py
@@ -0,0 +1,4 @@
+import parser
+
+if __name__ == '__main__':
+	G = parser.parseFile("paris_54000.txt")
